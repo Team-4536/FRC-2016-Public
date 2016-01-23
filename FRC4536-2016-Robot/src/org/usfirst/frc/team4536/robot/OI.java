afdc053d6af0b0ec62b79b1569433e66e1631ad0
Liam <liam.tomsonmoylan@gmail.com>
2016-01-23 12:38:05 -0600

    Added 2 Joysticks

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/OI.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/OI.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4536.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import org.usfirst.frc.team4536.robot.commands.ExampleCommand;
 
@@ -34,5 +35,12 @@ public class OI {
     // Start the command when the button is released  and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenReleased(new ExampleCommand());
+	
+	
+	
+	
+	//Joysticks
+	public static Joystick mainStick = new Joystick(RobotMap.MAIN_STICK);
+	public static Joystick secondaryStick = new Joystick(RobotMap.SECONDARY_STICK);
 }
 
