e1ae32e24d1bb6ec678b4878a84426d8d3a7d009
Jeremy <jeremytheguy3rd@gmail.com>
2016-01-23 11:49:02 -0600

    Smart Dashboard Command

---
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/SmartDashboardCommand.java
@@ -0,0 +1,50 @@
+package org.usfirst.frc.team4536.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team4536.robot.Utilities;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class SmartDashboardCommand extends Commandbase {
+	
+	/**
+	 * @author Jeremy
+	 */
+	public SmartDashboardCommand() {}
+	
+	/**
+	 * @author Jeremy
+	 */
+	protected void initialize() {
+		
+    }
+	
+	/**
+	 * @author Jeremy
+	 */
+    protected void execute() {
+    	
+    }
+    
+    /**
+	 * @author Jeremy
+	 * @return Whether the command is safe to be finished (true if it can be finished)
+	 */
+    protected boolean isFinished() {
+        return false;
+    }
+    
+    /**
+	 * @author Jeremy
+	 */
+    protected void end() {
+    	
+    }
+    
+    /**
+	 * @author Jeremy
+	 */
+    protected void interrupted() {
+    	end();
+    }
+
+}
