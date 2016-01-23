b2bff7f7beee3659932451ed6c195945f81facd8
Paul <paulmacgregor98@gmail.com>
2016-01-23 12:52:29 -0600

    Added a few constants to few in later.

---
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/Constants.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team4536.robot;
+
+public class Constants {
+	
+	/*---------------------------------------Drive Train--------------------------------*/
+
+	public static final double SPEED_CURVE = 0.0;
+	
+	/*---------------------------------------Joystick-----------------------------------*/
+	
+	public static final double DEAD_ZONE = 0.0;
+}
