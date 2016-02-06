bb2d017832dca53d58f95405d7d14992b33d674b
Mairead <magical.magical.me@gmail.com>
2016-02-06 13:52:14 -0600

    Has the beginnings of a turnTo command with a rectangle profile, it's not complete but since the meeting's done and this is not my computer I need to commit.

---
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/RectangleProfileForTurning.java
@@ -0,0 +1,75 @@
+package org.usfirst.frc.team4536.robot;
+
+public class RectangleProfileForTurning {
+
+	public double angle;
+	public double timeNeeded;
+	public double throttle;
+	public double desiredMaxAngularVelocity;
+	
+	/**
+	 * @author Mairead
+	 * @param angle The angle the robot should turn
+	 * (ex: if the robot's at 20 degrees and angle = 50, it will turn to 70 degrees)
+	 * @param desiredMaxAngularVelocity The speed the robot should be turning at in degrees per second, always positive
+	 */
+	public RectangleProfileForTurning (double angle, double desiredMaxAngularVelocity) {
+
+		this.desiredMaxAngularVelocity = desiredMaxAngularVelocity;
+		this.angle = angle;
+		if(angle > 0){
+			throttle = desiredMaxAngularVelocity / Constants.DRIVE_TRAIN_MAX_ANGULAR_VELOCITY;
+		} // Converts the desired velocity into a throttle value to send to the motors
+		if(angle < 0){
+			throttle = -desiredMaxAngularVelocity / Constants.DRIVE_TRAIN_MAX_ANGULAR_VELOCITY;
+		}
+		timeNeeded = Math.abs(angle / desiredMaxAngularVelocity); // Uses physics to calculate the time needed
+
+	}
+
+	/**
+	 * @author Mairead
+	 * @param time The amount of time since the profile has started
+	 * @returns The throttle the robot should be at
+	 */
+	public double throttle(double time) {
+
+		if (time > 0 && time < timeNeeded) {
+			return throttle;
+		}
+
+		else
+			return 0;
+	}
+	
+	/**
+	 * @author Mairead
+	 * @param time The amount of time since the profile has started
+	 * @returns The veloctiy the robot should be at
+	 */
+	public double idealRate(double time) {
+		
+		if (time > 0 && time < timeNeeded) {
+			return desiredMaxAngularVelocity;
+		} else
+			return 0;
+	}
+
+	/**
+	 * @author Mairead
+	 * @param time The amount of time since the profile has started
+	 * @returns The distance the robot should be at
+	 */
+	public double idealDistance(double time) {
+		
+		if (time > 0 && time < timeNeeded && angle > 0) {
+			return desiredMaxAngularVelocity * time;
+		} 
+		if (time > 0 && time < timeNeeded && angle < 0) {
+			return desiredMaxAngularVelocity * -time;
+		} 
+		else
+			return angle;
+	}
+
+}
---
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/TurnTo.java
@@ -0,0 +1,64 @@
+package org.usfirst.frc.team4536.robot.commands;
+
+import org.usfirst.frc.team4536.robot.Utilities;
+import org.usfirst.frc.team4536.robot.RectangleProfileForTurning;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ *
+ */
+public class TurnTo extends CommandBase {
+	
+	double desiredAngle;
+	double currentAngle;
+	double difference;
+	double time;
+	
+	Timer timer;
+	RectangleProfileForTurning rectangle;
+
+
+    public TurnTo(double desiredAngle, double velocity) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(driveTrain);
+    	
+    	currentAngle = driveTrain.getGyroAngle();
+    	difference = Utilities.angleDifference(currentAngle, desiredAngle);
+    	rectangle = new RectangleProfileForTurning(difference, velocity);
+    	
+    	timer.reset();
+    	timer.start();
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	
+    	
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	time = timer.get();
+    	
+    	driveTrain.arcadeDrive(0, rectangle.throttle(time));
+    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
