1ed1d71d650cf7aeb164e880efbd9079a9e6a679
Mairead <magical.magical.me@gmail.com>
2016-02-06 10:19:12 -0600

    Added a tangleTest button to drive the rectangle profile.

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/OI.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/OI.java
@@ -11,32 +11,6 @@ import org.usfirst.frc.team4536.robot.commands.*;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-    
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
 	
 	
 	/*----------------Initializations------------------*/
@@ -49,6 +23,7 @@ public class OI {
 	public static Button intake; 
 	public static Button eject;
 	public static Button hold;
+	public static Button tangleTest;
 	
 	/**
 	 * @author Liam
@@ -59,6 +34,7 @@ public class OI {
 		intake = new JoystickButton(secondaryStick, 5);
 		eject = new JoystickButton(secondaryStick, 6);
 		hold = new JoystickButton(secondaryStick, 7);
+		tangleTest = new JoystickButton(mainStick, 11);
 		
 		
 		/*-------------------Actions------------------------*/
@@ -66,6 +42,34 @@ public class OI {
 		intake.whenPressed(new IntakeBall());
 		eject.whenPressed(new EjectBall());
 		hold.whenPressed(new HoldBall());
+		tangleTest.whenPressed(new DriveRectangleProfile(-2,1));
 	}
 }
+
+////CREATING BUTTONS
+// One type of button is a joystick button which is any button on a joystick.
+// You create one by telling it which joystick it's on and which button
+// number it is.
+// Joystick stick = new Joystick(port);
+// Button button = new JoystickButton(stick, buttonNumber);
+
+// There are a few additional built in buttons you can use. Additionally,
+// by subclassing Button you can create custom triggers and bind those to
+// commands the same as any other Button.
+
+//// TRIGGERING COMMANDS WITH BUTTONS
+// Once you have a button, it's trivial to bind it to a button in one of
+// three ways:
+
+// Start the command when the button is pressed and let it run the command
+// until it is finished as determined by it's isFinished method.
+// button.whenPressed(new ExampleCommand());
+
+// Run the command while the button is being held down and interrupt it once
+// the button is released.
+// button.whileHeld(new ExampleCommand());
+
+// Start the command when the button is released  and let it run the command
+// until it is finished as determined by it's isFinished method.
+// button.whenReleased(new ExampleCommand());
 
