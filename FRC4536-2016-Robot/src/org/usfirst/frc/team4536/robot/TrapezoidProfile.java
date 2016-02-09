8eb43f6f7620f2a00b944faf50180b56344bf9c4
Liam <liam.tomsonmoylan@gmail.com>
2016-02-08 21:03:59 -0600

    So much, so close

---
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/TrapezoidProfile.java
@@ -0,0 +1,112 @@
+package org.usfirst.frc.team4536.robot;
+import java.lang.Math;
+
+/**
+ * 
+ * @author Liam
+ *
+ */
+public class TrapezoidProfile {
+
+	public double distance;
+	public double timeNeeded;
+	public double desiredMaxSpeed;
+	public double desiredMaxAcceleration;
+	public double criticalDistance; //This determines whether the profile is a triangle or a trapezoid.
+	public double criticalTime; //This is the time it takes to reach maxSpeed
+	public boolean triangle;
+	
+	public TrapezoidProfile (double distance, double maxSpeed, double maxAcceleration) {
+		
+		this.distance = distance;
+		this.desiredMaxSpeed = maxSpeed;
+		this.desiredMaxAcceleration = maxAcceleration;
+		
+		criticalTime = this.desiredMaxSpeed/this.desiredMaxAcceleration;
+		criticalDistance = criticalTime * this.desiredMaxSpeed/2;
+		
+		if (this.distance > criticalDistance) {
+			
+			triangle = false;
+		}
+		else {
+			
+			triangle = true;
+		}
+		
+		if (triangle) {
+			
+			this.timeNeeded = 2*Math.sqrt(Math.abs(this.distance/this.desiredMaxAcceleration));
+		}
+		else {
+			
+			this.timeNeeded = (2*criticalTime) + (Math.abs(this.distance) - criticalDistance/this.desiredMaxSpeed);
+		}
+	}
+		
+		/**
+		 * @author Liam
+		 * @param time The amount of time since the profile has started
+		 * @returns The throttle the robot should be at
+		 */
+		public double throttle(double time) {
+
+			return idealVelocity(time)/Constants.DRIVE_TRAIN_MAX_VELOCITY;
+		}
+		
+		/**
+		 * @author Liam
+		 * @param time The amount of time since the profile has started
+		 * @returns The veloctiy the robot should be at
+		 */
+		public double idealVelocity(double time) {
+			
+			double velocity;
+			
+			if (triangle) {
+				
+				if(time <= timeNeeded/2 && time > 0) {
+					
+					velocity =  this.desiredMaxAcceleration*time;
+				}
+				else if (time > timeNeeded/2 && time <= timeNeeded){
+					
+					double maxTriangleVelocity = this.desiredMaxAcceleration*timeNeeded/2;
+					
+					velocity = this.desiredMaxAcceleration*time + 2*maxTriangleVelocity;
+				}
+				else {
+					
+					velocity = 0.0;
+				}
+			}
+			else {//trapezoid
+				
+				if(time <= criticalTime && time >= 0) {//0 to max velocity
+					
+					velocity = this.desiredMaxAcceleration*time;
+				}
+				else if (time > criticalTime && time < (timeNeeded - criticalTime)) {//max velocity
+					
+					velocity = this.desiredMaxSpeed;
+				}
+				else if (time >= (timeNeeded - criticalTime) && time <= timeNeeded) {//max velocity to 0
+					
+					velocity = this.desiredMaxAcceleration*(timeNeeded - time);
+				}
+				else {//Garbage
+					
+					velocity = 0;
+				}
+			}
+			
+			if (distance < 0) {
+				
+				return -velocity;
+			}
+			else {
+				
+				return velocity;
+			}
+		}
+}
---
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/DriveTrapezoidProfile.java
@@ -0,0 +1,68 @@
+package org.usfirst.frc.team4536.robot.commands;
+
+import org.usfirst.frc.team4536.robot.TrapezoidProfile;
+import org.usfirst.frc.team4536.robot.Utilities;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *@author Mairead
+ */
+public class DriveTrapezoidProfile extends CommandBase {
+	
+	Timer timer = new Timer();
+	double time;
+	TrapezoidProfile trapezoid;
+	public double maxVelocity;
+	
+	/**
+	 * @author Mairead
+	 * @param distance The desired distance the robot should travel
+	 * @param desiredMaxVelocity The speed the robot should be traveling at
+	 */
+
+    public DriveTrapezoidProfile(double distance, double maxVelocity, double maxAcceleration) {
+
+    	requires(driveTrain);
+    	trapezoid = new TrapezoidProfile(distance, maxVelocity, maxAcceleration);
+    	this.maxVelocity = maxVelocity;
+    }
+
+ // Called just before this Command runs the first time
+    protected void initialize() {
+    	timer.reset();
+    	timer.start();
+    	driveTrain.resetRightEncoder();
+    	driveTrain.resetGyro();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	
+    	time = timer.get();
+    	
+    	driveTrain.arcadeDrive(trapezoid.throttle(time), 0);
+    	//Ask Caleb or Mairead on the implementation of feedforward+feedback
+    	
+    	System.out.println(driveTrain.getRightEncoder()/12);
+    	//Since getDistance is in feet, you have to divide by 12 to inches
+    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+
+    		return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	driveTrain.arcadeDrive(0, 0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
