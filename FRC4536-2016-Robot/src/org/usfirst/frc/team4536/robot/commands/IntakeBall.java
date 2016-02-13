fd0dd129ff89c75ec48f05dd978837803775b3e0
Liam <liam.tomsonmoylan@gmail.com>
2016-02-13 17:59:52 -0600

    Overloaded constructor to allow the setting of the speed

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/IntakeBall.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/IntakeBall.java
@@ -11,15 +11,26 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class IntakeBall extends CommandBase {
 	
+	private double speed = Constants.INTAKE_SPEED;
+	
     public IntakeBall() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(intake);
     	requires(maxUltra);
     }
+    
+    public IntakeBall(double speed) {
+    	
+    	this();
+    	
+    	this.speed = speed;
+    }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	
+    	setTimeout(1.0);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -28,14 +39,14 @@ public class IntakeBall extends CommandBase {
     		intake.setThrottle(0);
     	}
     	else {
-    		intake.setThrottle(Constants.INTAKE_SPEED);
+    		intake.setThrottle(speed);
     	}
     	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return isTimedOut();
     }
 
     // Called once after isFinished returns true
@@ -47,5 +58,7 @@ public class IntakeBall extends CommandBase {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	
+    	end();
     }
 }
