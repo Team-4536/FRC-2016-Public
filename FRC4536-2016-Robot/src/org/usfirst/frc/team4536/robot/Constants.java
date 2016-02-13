12f0b97bcb1ac8df14f3bd8cf19bda4821111bc0
Liam <liam.tomsonmoylan@gmail.com>
2016-02-13 13:22:36 -0600

    Replaced Variables with constants

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/Constants.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/Constants.java
@@ -10,7 +10,8 @@ public class Constants {
 	public static final double FORWARD_STICTION = 0.07; //No climber with fully charged battery
 	public static final double TURN_STICTION = 0.3; //No climber with fully charged battery, one motor not functional
 	public static final double DRIVE_TRAIN_MAX_VELOCITY = 13.; //Feet per second, measured on blocks, 12.68 Battery Voltage
-	public static final double TRAPEZOID_FORWARD_PROPORTIONALITY = 0.005;
+	public static final double TRAPEZOID_FORWARD_PROPORTIONALITY = 0.05;
+	public static final double TRAPEZOID_FORWARD_GYRO_PROPORTIONALITY = -0.015;
 	//Zenith
 	//TODO change Zenith stiction back
 	public static final double ZENITH_FORWARD_STICTION = 0.00; // 0.04
