fb0c0d90d5b6c3ffb904be3660333ba68fadc174
Liam <liam.tomsonmoylan@gmail.com>
2016-02-13 17:55:58 -0600

    Added timeout for pickupBoulder and accel value for reach outer works

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/Constants.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/Constants.java
@@ -7,43 +7,44 @@ public class Constants {
 	/*---------------------------------------Drive Train--------------------------------*/
 
 	public static final double SPEED_CURVE_STRAIGHT = 2.4;
-	public static final double SPEED_CURVE_TURN = 2.0;
+	public static final double SPEED_CURVE_TURN = 1.5;
 	public static final double SPEED_CURVE = 2.4;
 	public static final double SPEED_LIMIT = 1;
-	public static final double ACCEL_LIMIT_DRIVE = 0.2;
-	public static final double ACCEL_LIMIT_INTAKE = 2.0;
+	public static final double ACCEL_LIMIT_DRIVE = 0.1;
+	public static final double ACCEL_LIMIT_INTAKE = 0.1;
 	public static final double FORWARD_STICTION = 0.07; //No climber with fully charged battery
 	public static final double TURN_STICTION = 0.3; //No climber with fully charged battery, one motor not functional
 	
 	public static final double DRIVE_TRAIN_MAX_VELOCITY = 13.; //Feet per second, measured on blocks, 12.68 Battery Voltage
 	public static final double TRAPEZOID_FORWARD_PROPORTIONALITY = 0.05;
 	public static final double TRAPEZOID_FORWARD_GYRO_PROPORTIONALITY = -0.015;
+	
 	//Zenith
-	//TODO change Zenith stiction back
 	public static final double ZENITH_DRIVE_TRAIN_MAX_ANGULAR_VELOCITY = 400;
 	public static final double ZENITH_FORWARD_STICTION = 0.00; // 0.04
 	public static final double ZENITH_TURN_STICTION = 0.1;
 	public static final double ZENITH_DRIVE_TRAIN_MAX_VELOCITY = 4.1; //In feet per seconds
 	/*---------------------------------------Joystick-----------------------------------*/
 	
-	public static final double DEAD_ZONE = 0.1;
+	public static final double DEAD_ZONE = 0.13;
 	
 	/*---------------------------------------Encoders-----------------------------------*/
 	
-	public static final double DRIVE_ENCODER_PROPORTIONALITY_CONSTANT = 19.0; //Ticks per inch
+	public static final double DRIVE_ENCODER_PROPORTIONALITY_CONSTANT = 14.; //Ticks per inch
 	
 	/*-------------------------------------AutoMode Time Outs---------------------------*/
 	
 	public static final double REACH_DEFENSE_TIME_OUT = 0.9;
+	public static final double PICK_UP_BALL_TIME_OUT = 0.1;
 	
 	/*-------------------------------------Auto Mode Distances-----------------------*/
 	
-	public static final double REACH_DEFENSE_DISTANCE = 2.0;
-	public static final double REACH_DEFENSE_VELOCITY = 1.0;
+	public static final double REACH_DEFENSE_DISTANCE = 3.0; //Feet
+	public static final double REACH_DEFENSE_VELOCITY = 1.0; //Feet per second
+	public static final double REACH_DEFENSE_ACCELERATION = 2.0; // Feet per second squared
 	
 	/*---------------------------------------Intake-------------------------------------*/
 	
-	//TODO change these value from test values to values for the intake 
 	public static final double INTAKE_SPEED = 1.0;
 	public static final double EJECT_SPEED = -1.0;
 	public static final double HOLD_SPEED = 0.00;
