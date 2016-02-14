0b9772dfc090823673d008dfe7c17ac4ada8d246
Liam <liam.tomsonmoylan@gmail.com>
2016-02-14 13:10:57 -0600

    Fixed indentation. Shift + tab indents highlighted code left

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/subsystems/ScissorLift.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/subsystems/ScissorLift.java
@@ -17,41 +17,41 @@ public class ScissorLift extends Subsystem {
 	VictorSP scissorLift;
 	Relay relay;
 	
-		public ScissorLift(int motorChannel){
+	public ScissorLift(int motorChannel){
+
+		scissorLift = new VictorSP(motorChannel);
+		relay = new Relay(RobotMap.SCISSOR_RELAY, Relay.Direction.kForward);
+	}
+		
+	/**
+	 * @author Liam
+	 * @param Throttle the value [-1, 1] sent to the motor. Negative values make it climb.
+	 * Positive values make it go down.
+	 */
+	public void driveLift(double throttle) {
+		
+		scissorLift.set(throttle);
+    }
 	
-			scissorLift = new VictorSP(motorChannel);
-			relay = new Relay(RobotMap.SCISSOR_RELAY, Relay.Direction.kForward);
+	/**
+	 * @author Sheila
+	 */
+	public void relayOn() {
+		relay.set(Relay.Value.kOn);
+	}
+	public void relayOff() {
+		relay.set(Relay.Value.kOff);
+	}
+	public void relayFlip() {
+		if (relay.get() == Relay.Value.kOn){
+			relayOn();
+		} else {
+			relayOff();
 		}
-		
-		/**
-		 * @author Liam
-		 * @param Throttle the value [-1, 1] sent to the motor. Negative values make it climb.
-		 * Positive values make it go down.
-		 */
-		public void driveLift(double throttle) {
-			scissorLift.set(throttle);
-			
-	    }
-		
-		/**
-		 * @author Sheila
-		 */
-		public void relayOn() {
-			relay.set(Relay.Value.kOn);
-		}
-		public void relayOff() {
-			relay.set(Relay.Value.kOff);
-		}
-		public void relayFlip() {
-			if (relay.get() == Relay.Value.kOn){
-				relayOn();
-			} else {
-				relayOff();
-			}
-		}
-		public void safeDrive(double throttle) {
-			driveLift(Utilities.limit(throttle, -1.0, 0.0));
-		}
+	}
+	public void safeDrive(double throttle) {
+		driveLift(Utilities.limit(throttle, -1.0, 0.0));
+	}
 			
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
