79c9548ffa5355dae69879fdf63e3c6cf577d4f6
Liam <liam.tomsonmoylan@gmail.com>
2016-02-14 10:39:56 -0600

    Added units for all but ZENITH_MAX_ANGULAR_VELOCITY and ULTRA_CONVERSION

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/Constants.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/Constants.java
@@ -6,27 +6,28 @@ public class Constants {
 	
 	/*---------------------------------------Drive Train--------------------------------*/
 
-	public static final double SPEED_CURVE_STRAIGHT = 2.4;
-	public static final double SPEED_CURVE_TURN = 1.5;
-	public static final double SPEED_CURVE = 2.4;
-	public static final double SPEED_LIMIT = 1;
-	public static final double ACCEL_LIMIT_DRIVE = 0.1;
-	public static final double ACCEL_LIMIT_INTAKE = 0.1;
+	public static final double SPEED_CURVE_STRAIGHT = 2.4; // Unitless exponent
+	public static final double SPEED_CURVE_TURN = 1.5; // Unitless exponent
+	public static final double SPEED_CURVE = 2.4; // Unitless exponent
+	public static final double SPEED_LIMIT = 1; // In throttle
+	public static final double ACCEL_LIMIT_DRIVE = 0.1; // The time it takes to reach full speed in seconds
+	public static final double ACCEL_LIMIT_INTAKE = 0.1; // The time it takes to reach full speed in seconds
 	public static final double FORWARD_STICTION = 0.07; //No climber with fully charged battery
 	public static final double TURN_STICTION = 0.3; //No climber with fully charged battery, one motor not functional
 	
 	public static final double DRIVE_TRAIN_MAX_VELOCITY = 13.; //Feet per second, measured on blocks, 12.68 Battery Voltage
-	public static final double TRAPEZOID_FORWARD_PROPORTIONALITY = 0.05;
-	public static final double TRAPEZOID_FORWARD_GYRO_PROPORTIONALITY = -0.015;
+	public static final double TRAPEZOID_FORWARD_PROPORTIONALITY = 0.05; // Unitless correction factor
+	public static final double TRAPEZOID_FORWARD_GYRO_PROPORTIONALITY = -0.015; // Unitless correction factor
 	
 	//Zenith
 	public static final double ZENITH_DRIVE_TRAIN_MAX_ANGULAR_VELOCITY = 400;
 	public static final double ZENITH_FORWARD_STICTION = 0.00; // 0.04
-	public static final double ZENITH_TURN_STICTION = 0.1;
+	public static final double ZENITH_TURN_STICTION = 0.1; // In throttle
 	public static final double ZENITH_DRIVE_TRAIN_MAX_VELOCITY = 4.1; //In feet per seconds
+	
 	/*---------------------------------------Joystick-----------------------------------*/
 	
-	public static final double DEAD_ZONE = 0.13;
+	public static final double DEAD_ZONE = 0.13; // In throttle
 	
 	/*---------------------------------------Encoders-----------------------------------*/
 	
@@ -34,8 +35,8 @@ public class Constants {
 	
 	/*-------------------------------------AutoMode Time Outs---------------------------*/
 	
-	public static final double REACH_DEFENSE_TIME_OUT = 0.9;
-	public static final double PICK_UP_BALL_TIME_OUT = 0.1;
+	public static final double REACH_DEFENSE_TIME_OUT = 0.9; // In seconds
+	public static final double PICK_UP_BALL_TIME_OUT = 0.1; // In seconds
 	
 	/*-------------------------------------Auto Mode Distances-----------------------*/
 	
@@ -45,18 +46,20 @@ public class Constants {
 	
 	/*---------------------------------------Intake-------------------------------------*/
 	
-	public static final double INTAKE_SPEED = 1.0;
-	public static final double EJECT_SPEED = -1.0;
-	public static final double HOLD_SPEED = 0.00;
+	public static final double INTAKE_SPEED = 1.0; // In throttle
+	public static final double EJECT_SPEED = -1.0; // In throttle
+	public static final double HOLD_SPEED = 0.00; // In throttle
 	public static final double INTAKE_STICTION = 0.06; //Full battery
-	public static final double INTAKE_SPEED_CURVE = 1;
+	public static final double INTAKE_SPEED_CURVE = 1; // Unitless exponent
 	
 	public static final double MAX_ULTRA_CONVERSION = 92.0;
 	
 	/*-----Access Values-----*/
 	/*@author Liam
 	 * If you have displayed a number on the SmartDashboard you may
-	 * change it and access its value.
+	 * change it and access its value. This allows for rapid development
+	 * of constants because we can tweak values without re-deploying code.
+	 * We can also run test cases quickly, all from the SmartDashboard.
 	 */
 	
 	public static double variable1 = 0.0;
