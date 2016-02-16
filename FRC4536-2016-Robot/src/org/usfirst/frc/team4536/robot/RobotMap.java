a7565824c547a67ff7067a5a15b4f391e967f490
audreylemeur <alemeur001@stpaul.k12.mn.us>
2016-02-16 16:51:52 -0600

    Rearranged groupings in RobotMap (Issue #26)

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/RobotMap.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/RobotMap.java
@@ -15,40 +15,28 @@ public class RobotMap {
     public static final int RIGHT_MOTOR_FRONT = 2;
     public static final int RIGHT_MOTOR_BACK = 3;
     
+    public static final int INTAKE_CHANNEL = 4;
+    
+    public static final int SCISSOR_MOTOR = 5;
+    
     /*-------------------------------------Joystick Ports---------------------------------*/
     public static final int MAIN_STICK = 0; // (Also the left stick for tank drive)
     public static final int SECONDARY_STICK = 1; // (Also the right stick for tank drive)
     public static final int TERTIARY_STICK = 2; //Scissor Lift
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
-    
-	public static int leftSolenoid = 2; //Zenith = 2; Onyx = 4?
-	public static int rightSolenoid = 3; //Zenith = 3; Onyx = 7?
-	
-	/*-------------------------------------Intake-----------------------------------------*/
-	
-	public static final int INTAKE_CHANNEL = 4;
-	
-	/*-------------------------------------Scissor Lift Winch-----------------*/
-	
-	public static final int SCISSOR_MOTOR = 5;
-	
 
 	/*-------------------------------------Relay Solenoid-----------------*/
 	
 	public static final int SCISSOR_RELAY = 0; //these go in the relay port, 
 	public static final int INTAKE_RELAY = 1; //electricians; not PWM or DIM 
 	
-    /*-------------------------------------Sensor Channels---------------------------------*/	
+    /*-------------------------------------Digital Sensor Channels---------------------------------*/	
 	
 	public static final int LEFT_ENCODER_CHANNEL_A = 8; //Set of 3 wires in one port for left encoder
 	public static final int LEFT_ENCODER_CHANNEL_B = 9; //Set of 3 wires in one port for left encoder
 	public static final int RIGHT_ENCODER_CHANNEL_A = 6; //Set of 3 wires in one port for right encoder
 	public static final int RIGHT_ENCODER_CHANNEL_B = 7; //Set of 3 wires in one port for right encoder
 
+	 /*-------------------------------------Analog Sensor Channels---------------------------------*/
 	public static final int GYRO_CHANNEL = 0;
 	
 	public static final int ULTRA_CHANNEL = 1;
