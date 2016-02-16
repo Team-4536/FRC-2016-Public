2896b339cf6c446b09d41c3d6ee74123e47b8532
audreylemeur <alemeur001@stpaul.k12.mn.us>
2016-02-15 18:27:58 -0600

    Create CrossRockWall.java

---
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/CrossRockWall.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team4536.robot.commands;
+
+import org.usfirst.frc.team4536.robot.Constants;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+
+public class CrossRockWall extends CommandGroup {
+    
+public  CrossRockWall(boolean foward) {
+    	
+		DriveTrapezoidProfile crossRockWall;
+		
+    	if (foward){
+        	crossRockWall = new DriveTrapezoidProfile(Constants.CROSS_ROCKWALL_DISTANCE, Constants.CROSS_ROCKWALL_VELOCITY, Constants.CROSS_ROCKWALL_ACCEL_LIMIT);
+        	double maxTime = crossRockWall.getNeededTime() + 2;
+    		addSequential(crossRockWall, maxTime);
+    	}else{
+        	crossRockWall = new DriveTrapezoidProfile(-Constants.CROSS_ROCKWALL_DISTANCE, Constants.CROSS_ROCKWALL_VELOCITY, Constants.CROSS_ROCKWALL_ACCEL_LIMIT);
+        	double maxTime = crossRockWall.getNeededTime() + 2;
+    		addSequential(crossRockWall, maxTime);
+    	}
+}
+}
