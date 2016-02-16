a4861b9f35ab50a9fc0c32dca22063365c322da0
audreylemeur <alemeur001@stpaul.k12.mn.us>
2016-02-16 16:25:28 -0600

    Added extra time constants (Issue #39)

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/CrossLowBar.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/CrossLowBar.java
@@ -15,11 +15,11 @@ public class CrossLowBar extends CommandGroup {
 		
     	if (foward){
         	crossLowBar = new DriveTrapezoidProfile(Constants.CROSS_LOWBAR_DISTANCE, Constants.CROSS_LOWBAR_VELOCITY, Constants.CROSS_LOWBAR_ACCEL_LIMIT);
-        	double maxTime = crossLowBar.getNeededTime() + 1;
+        	double maxTime = crossLowBar.getNeededTime() + Constants.CROSS_LOWBAR_EXTRA_TIME;
     		addSequential(crossLowBar, maxTime);
     	}else{
         	crossLowBar = new DriveTrapezoidProfile(-Constants.CROSS_LOWBAR_DISTANCE, Constants.CROSS_LOWBAR_VELOCITY, Constants.CROSS_LOWBAR_ACCEL_LIMIT);
-        	double maxTime = crossLowBar.getNeededTime() + 1;
+        	double maxTime = crossLowBar.getNeededTime() + Constants.CROSS_LOWBAR_EXTRA_TIME;
     		addSequential(crossLowBar, maxTime);
     	}
     	
