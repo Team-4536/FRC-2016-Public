ef7cba7c0b44fc94f0a70ba29ca1b027e5fe99a3
audreylemeur <alemeur001@stpaul.k12.mn.us>
2016-02-16 19:48:46 -0600

    New command for switching defenses

---
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/CrossDefense.java
@@ -0,0 +1,55 @@
+package org.usfirst.frc.team4536.robot.commands;
+
+import org.usfirst.frc.team4536.robot.Utilities;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class CrossDefense extends CommandGroup {
+    
+    public  CrossDefense(int defense, boolean forward) {
+    	
+    	Utilities.Defense defenseEnum = Utilities.Defense.values()[defense];
+    	
+    	switch(defenseEnum){
+      
+    	case LOW_BAR:
+    	  
+    		new CrossLowBar(forward).start();
+    		
+    	break;
+    	 
+    	case MOAT:
+    		
+    		new CrossMoat(forward).start();
+    		
+    	break;
+    	
+    	case ROCK_WALL:
+    		
+    		new CrossRockWall(forward).start();
+    		
+    	break;
+    	
+    	case ROUGH_TERRAIN:
+    		
+    		new CrossRoughTerrain(forward).start();
+    		
+    	break;
+    	
+    	case RAMPARTS:
+    		
+    		//TODO Add ramparts to cross defend command group
+    		
+    	break;
+    	
+    	default:
+    		
+    		new DoNothing().start();
+    		
+    	break;
+      }
+    }
+}
