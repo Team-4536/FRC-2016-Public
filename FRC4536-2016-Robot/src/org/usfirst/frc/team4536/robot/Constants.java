bc5b84bf3015c042394b08e346261d9c33cc4a12
Liam <liam.tomsonmoylan@gmail.com>
2016-02-17 21:04:08 -0600

    Updated auto constants

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/Constants.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/Constants.java
@@ -16,6 +16,7 @@ public class Constants {
 	public static final double TURN_STICTION = 0.3; //No climber with fully charged battery, one motor not functional
 	
 	public static final double DRIVE_TRAIN_MAX_VELOCITY = 13.; //Feet per second, measured on blocks, 12.68 Battery Voltage
+	public static final double DRIVE_TRAIN_MAX_ANGUAR_VELOCITY = 400; // Degrees per second, measured on carpet, 12.83 Battery Voltage
 	public static final double TRAPEZOID_FORWARD_PROPORTIONALITY = 0.05; // units of throttle per inch
 	public static final double TRAPEZOID_FORWARD_GYRO_PROPORTIONALITY = -0.03; // Units of throttle per degree
 	//about the max the robot will reasonably allow is -0.1 (damped oscillation at end)
@@ -49,7 +50,7 @@ public class Constants {
 	public static final double REACH_DEFENSE_VELOCITY = 1.0; //Feet per second
 	public static final double REACH_DEFENSE_ACCELERATION = 2.0; // Feet per second squared
 	
-	public static final double CROSS_LOWBAR_DISTANCE = 10.0;
+	public static final double CROSS_LOWBAR_DISTANCE = 13.0;
 	public static final double CROSS_LOWBAR_VELOCITY = 3.0;
 	public static final double CROSS_LOWBAR_ACCEL_LIMIT = 3.0;
 	
@@ -57,16 +58,16 @@ public class Constants {
 	public static final double CROSS_ROCKWALL_VELOCITY = 5.0;
 	public static final double CROSS_ROCKWALL_ACCEL_LIMIT = 3.0;
 
-	public static final double CROSS_ROUGHTERRAIN_DISTANCE = 14.5;
+	public static final double CROSS_ROUGHTERRAIN_DISTANCE = 13;
 	public static final double CROSS_ROUGHTERRAIN_VELOCITY = 3.0;
 	public static final double CROSS_ROUGHTERRAIN_ACCEL_LIMIT = 3.0;
 	
-	public static final double CROSS_RAMPARTS_DISTANCE = 18.0;
+	public static final double CROSS_RAMPARTS_DISTANCE = 15.0;
 	public static final double CROSS_RAMPARTS_VELOCITY = 5.0;
 	public static final double CROSS_RAMPARTS_ACCEL_LIMIT = 3.0;
 	public static final double CROSS_RAMPARTS_GYRO_PROPORTIONALITY = -0.05;
 	
-	public static final double CROSS_MOAT_DISTANCE = 18.0;
+	public static final double CROSS_MOAT_DISTANCE = 16.0;
 	public static final double CROSS_MOAT_VELOCITY = 8.0;
 	public static final double CROSS_MOAT_ACCEL_LIMIT = 6.0;
 	
@@ -93,6 +94,7 @@ public class Constants {
 	public static double variable3 = 0.0;
 	public static double variable4 = 0.0;
 	public static double variable5 = 0.0;
+	public static boolean variable6 = true;
 	
 	public static void displaySmartDashboard() {
 		
@@ -101,6 +103,7 @@ public class Constants {
 		SmartDashboard.putNumber("Variable 3: ", 0.0);
 		SmartDashboard.putNumber("Variable 4: ", 0.0);
 		SmartDashboard.putNumber("Variable 5: ", 0.0);
+		SmartDashboard.putBoolean("Variable 6: ", true);
 	}
 	
 	public static void updateVariables() {
@@ -110,5 +113,6 @@ public class Constants {
 		variable3 = SmartDashboard.getNumber("Variable 3: ");
 		variable4 = SmartDashboard.getNumber("Variable 4: ");
 		variable5 = SmartDashboard.getNumber("Variable 5: ");
+		variable6 = SmartDashboard.getBoolean("Variable 6: ");
 	}
 }
