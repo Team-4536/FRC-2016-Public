ad81807a5dd2d2b7aa88d7fa0e9d175e449a3413
Liam <liam.tomsonmoylan@gmail.com>
2016-02-19 15:51:20 -0600

    DoNothing requires all subsystems, resets accelValues, sets motors to 0

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/DoNothing.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/DoNothing.java
@@ -7,12 +7,23 @@ package org.usfirst.frc.team4536.robot.commands;
 public class DoNothing extends CommandBase {
 
     public DoNothing() {
+    	
+    	requires(driveTrain);
+    	requires(intake);
+    	requires(scissorLift);
     }
     
     protected void initialize() {
+    	
+    	driveTrain.resetAccelValues();
+    	intake.resetAccelValues();
     }
     
     protected void execute() {
+    	
+    	driveTrain.arcadeDriveAccelLimit(0.0, 0.0);
+    	intake.setThrottleAccelLimited(0.0);
+    	scissorLift.driveLift(0.0);
     }
     
     protected boolean isFinished() {
