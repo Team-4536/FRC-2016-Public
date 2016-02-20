c67508ddda5f12d442477b1cf3d915285249ba14
Liam <liam.tomsonmoylan@gmail.com>
2016-02-20 15:48:19 -0600

    Added display of intake IR sensor

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/SmartDashboardCommand.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/SmartDashboardCommand.java
@@ -42,6 +42,7 @@ public class SmartDashboardCommand extends CommandBase {
 		SmartDashboard.putNumber("Drive Train Left Rate: ", driveTrain.getLeftRate());
 		SmartDashboard.putNumber("Previous Forward Throttle", driveTrain.oldForwardThrottle);
 		SmartDashboard.putNumber("Turn Rate in Degrees per Second: ", driveTrain.getYawRate());
+		SmartDashboard.putNumber("Ball Distance: ", intake.getdistance());
 		
 		/*-----Display NavX Values-----*/
 		
