f55be86c0a5a786b56143e955edaa4b4e731a6f0
audreylemeur <alemeur001@stpaul.k12.mn.us>
2016-02-20 13:16:09 -0600

    Commented (Issue #50)

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/CrossLowBar.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/CrossLowBar.java
@@ -3,16 +3,17 @@ package org.usfirst.frc.team4536.robot.commands;
 import org.usfirst.frc.team4536.robot.Constants;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-/**
- * 
- */
 public class CrossLowBar extends CommandGroup {
-    
-    public  CrossLowBar(boolean foward) {
+	
+	/**
+	 * @author Audrey
+	 * @param forward boolean if true, it goes forward if false, it goes backward
+	 */
+    public  CrossLowBar(boolean forward) {
     	
 		DriveTrapezoidProfile crossLowBar;
 		
-    	if (foward){
+    	if (forward){
         	crossLowBar = new DriveTrapezoidProfile(Constants.CROSS_LOWBAR_DISTANCE, Constants.CROSS_LOWBAR_VELOCITY, Constants.CROSS_LOWBAR_ACCEL_LIMIT);
         	double maxTime = crossLowBar.getNeededTime() + Constants.CROSS_LOWBAR_EXTRA_TIME;
     		addSequential(crossLowBar, maxTime);
