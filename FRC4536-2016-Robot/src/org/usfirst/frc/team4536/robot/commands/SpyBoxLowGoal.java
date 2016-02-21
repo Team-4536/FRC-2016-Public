2c230c0cbd316d7e05502dfbfa86b24efe4e609f
Liam <liam.tomsonmoylan@gmail.com>
2016-02-20 18:55:02 -0600

    Scores in low goal from the spy box

---
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/SpyBoxLowGoal.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team4536.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class SpyBoxLowGoal extends CommandGroup {
+    
+    public  SpyBoxLowGoal() {
+    	
+    	addParallel(new ReleaseIntake());
+    	addSequential(new DriveTrapezoidProfile(6.5, 10, 4));
+    	addSequential(new TurnTrapezoidProfile(-75, 150, 90));
+    	addSequential(new DriveTrapezoidProfile(5.3, 10, 4));
+    	addSequential(new EjectBoulderAccelLimited());
+    }
+}
