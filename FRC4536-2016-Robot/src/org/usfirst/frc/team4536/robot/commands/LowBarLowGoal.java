03b2907525a268cb4c1581b73de8b2bce938d183
Liam <liam.tomsonmoylan@gmail.com>
2016-02-22 20:26:40 -0600

    GOAAAAAAAAAAAAAALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL!

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/LowBarLowGoal.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/LowBarLowGoal.java
@@ -13,10 +13,10 @@ public class LowBarLowGoal extends CommandGroup {
     public  LowBarLowGoal() {
     	
     	addSequential(new CrossLowBar());
-    	addSequential(new TurnTrapezoidProfile(10));
-    	addSequential(new DriveTrapezoidProfile(Constants.variable1)); // 7
-    	addSequential(new TurnTrapezoidProfile(Constants.variable3)); // 50
-    	addSequential(new DriveTrapezoidProfile(Constants.variable2)); // 5
+    	addSequential(new TurnTrapezoidProfile(Constants.LOW_BAR_LOW_GOAL_FIRST_LEG_ANGLE));
+    	addSequential(new DriveTrapezoidProfile(Constants.LOW_BAR_LOW_GOAL_FIRST_LEG_DISTANCE)); // 7
+    	addSequential(new TurnTrapezoidProfile(Constants.LOW_BAR_LOW_GOAL_SECOND_LEG_ANGLE)); // 50
+    	addSequential(new DriveTrapezoidProfile(Constants.LOW_BAR_lOW_GOAL_SECOND_LEG_DISTANCE)); // 5
     	addSequential(new EjectBoulderAccelLimited());
     }
 }
