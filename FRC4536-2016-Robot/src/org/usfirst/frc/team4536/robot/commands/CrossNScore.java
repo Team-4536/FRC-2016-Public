a8d1318532eeba45a1584554a6092720dad6dd3f
Liam <liam.tomsonmoylan@gmail.com>
2016-02-22 22:05:02 -0600

    executes crossing then scoring routine from defense alignment position

---
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/CrossNScore.java
@@ -0,0 +1,50 @@
+package org.usfirst.frc.team4536.robot.commands;
+
+import org.usfirst.frc.team4536.robot.Utilities;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.team4536.robot.commands.DefenseAlignmentPosition;
+
+/**
+ *
+ */
+public class CrossNScore extends CommandGroup {
+	
+	private boolean orientation;
+    
+    public  CrossNScore(int defense, int pos) {
+    	
+    	orientation = false;
+    	
+		switch (defense) {
+		
+			case 5:
+				
+				addSequential(new CrossDefense(Utilities.Defense.ROUGH_TERRAIN, orientation));
+			break;
+			
+			case 6:
+				
+				addSequential(new CrossDefense(Utilities.Defense.ROCK_WALL, orientation));
+			break;
+	
+			case 7:
+				
+				addSequential(new CrossDefense(Utilities.Defense.MOAT, orientation));
+			break;
+				
+			case 8:
+				
+				addSequential(new CrossDefense(Utilities.Defense.RAMPARTS, orientation));
+				
+			break;
+			
+			default: 
+				
+				addSequential(new ReleaseIntake());
+			break;
+		}
+		
+		addSequential(new DefenseAlignmentPosition(pos));
+    }
+}
