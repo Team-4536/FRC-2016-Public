d76ccbd9331435c6501dca37f9b42eafe922e943
Liam <liam.tomsonmoylan@gmail.com>
2016-02-23 21:10:21 -0600

    Worked in ultrasonic logic

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/DefenseAlignmentPosition.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/DefenseAlignmentPosition.java
@@ -17,32 +17,68 @@ public class DefenseAlignmentPosition extends CommandGroup {
 	    		
 	        	addSequential(new TurnTrapezoidProfile(Constants.DEFENSE_ALIGNMENT2_ANGLE));
 	        	addSequential(new DriveTrapezoidProfile(Constants.DEFENSE_ALIGNMENT2_DISTANCE));
-	        	addSequential(new TurnTrapezoidProfile(-Constants.DEFENSE_ALIGNMENT2_ANGLE));
-	        	addSequential(new FinishScoringRoutine(Constants.DEFENSE_ALIGNMENT2_TURN));
+	        	
+	        	if (Constants.DEFENSE_ALIGNMENT2_ULTRA) {
+	        		
+	        		addSequential(new TurnTrapezoidProfile(-Constants.DEFENSE_ALIGNMENT2_ANGLE));
+	        		//TODO add ultra code here
+		        	addSequential(new FinishScoringRoutine(Constants.DEFENSE_ALIGNMENT2_TURN, Constants.DEFAULT_FINISHING_ANGLE));
+	        	}
+	        	else {
+	        		
+		        	addSequential(new FinishScoringRoutine(Constants.DEFENSE_ALIGNMENT2_TURN, Constants.DEFAULT_FINISHING_ANGLE+Constants.DEFENSE_ALIGNMENT2_ANGLE));
+	        	}
 	    	break;
 	    	
 	    	case 3:
 	    		
 	        	addSequential(new TurnTrapezoidProfile(Constants.DEFENSE_ALIGNMENT3_ANGLE));
 	        	addSequential(new DriveTrapezoidProfile(Constants.DEFENSE_ALIGNMENT3_DISTANCE));
-	        	addSequential(new TurnTrapezoidProfile(-Constants.DEFENSE_ALIGNMENT3_ANGLE));
-	        	addSequential(new FinishScoringRoutine(Constants.DEFENSE_ALIGNMENT3_TURN));
+	        	
+	        	if (Constants.DEFENSE_ALIGNMENT3_ULTRA) {
+	        		
+	        		addSequential(new TurnTrapezoidProfile(-Constants.DEFENSE_ALIGNMENT3_ANGLE));
+	        		//TODO add ultra code here
+		        	addSequential(new FinishScoringRoutine(Constants.DEFENSE_ALIGNMENT3_TURN, Constants.DEFAULT_FINISHING_ANGLE));
+	        	}
+	        	else {
+	        		
+		        	addSequential(new FinishScoringRoutine(Constants.DEFENSE_ALIGNMENT3_TURN, Constants.DEFAULT_FINISHING_ANGLE+Constants.DEFENSE_ALIGNMENT3_ANGLE));
+	        	}
 	    	break;
 	    	
 	    	case 4:
 	    		
 	        	addSequential(new TurnTrapezoidProfile(Constants.DEFENSE_ALIGNMENT4_ANGLE));
 	        	addSequential(new DriveTrapezoidProfile(Constants.DEFENSE_ALIGNMENT4_DISTANCE));
-	        	addSequential(new TurnTrapezoidProfile(-Constants.DEFENSE_ALIGNMENT4_ANGLE));
-	        	addSequential(new FinishScoringRoutine(Constants.DEFENSE_ALIGNMENT4_TURN));
+	        	
+	        	if (Constants.DEFENSE_ALIGNMENT4_ULTRA) {
+	        		
+	        		addSequential(new TurnTrapezoidProfile(-Constants.DEFENSE_ALIGNMENT4_ANGLE));
+	        		//TODO add ultra code here
+		        	addSequential(new FinishScoringRoutine(Constants.DEFENSE_ALIGNMENT4_TURN, Constants.DEFAULT_FINISHING_ANGLE));
+	        	}
+	        	else {
+	        		
+		        	addSequential(new FinishScoringRoutine(Constants.DEFENSE_ALIGNMENT4_TURN, Constants.DEFAULT_FINISHING_ANGLE-Constants.DEFENSE_ALIGNMENT4_ANGLE));
+	        	}
 	    	break;
 	    	
 	    	case 5:
 	    		
 	        	addSequential(new TurnTrapezoidProfile(Constants.DEFENSE_ALIGNMENT5_ANGLE));
 	        	addSequential(new DriveTrapezoidProfile(Constants.DEFENSE_ALIGNMENT5_DISTANCE));
-	        	addSequential(new TurnTrapezoidProfile(-Constants.DEFENSE_ALIGNMENT5_ANGLE));
-	        	addSequential(new FinishScoringRoutine(Constants.DEFENSE_ALIGNMENT5_TURN));
+	        	
+	        	if (Constants.DEFENSE_ALIGNMENT2_ULTRA) {
+	        		
+	        		addSequential(new TurnTrapezoidProfile(-Constants.DEFENSE_ALIGNMENT5_ANGLE));
+	        		//TODO add ultra code here
+		        	addSequential(new FinishScoringRoutine(Constants.DEFENSE_ALIGNMENT5_TURN, Constants.DEFAULT_FINISHING_ANGLE));
+	        	}
+	        	else {
+	        		
+		        	addSequential(new FinishScoringRoutine(Constants.DEFENSE_ALIGNMENT5_TURN, Constants.DEFAULT_FINISHING_ANGLE-Constants.DEFENSE_ALIGNMENT5_ANGLE));
+	        	}
 	    	break;
     	}
     }
