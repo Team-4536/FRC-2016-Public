319f9d4bd56b665d60ae3a4419c9428dfca079e3
audreylemeur <alemeur001@stpaul.k12.mn.us>
2016-03-10 19:53:12 -0600

    Not functional

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/CrossDefense.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/CrossDefense.java
@@ -1,55 +1,51 @@
 package org.usfirst.frc.team4536.robot.commands;
 
 import org.usfirst.frc.team4536.robot.Utilities;
+import org.usfirst.frc.team4536.robot.Utilities.Defense;
 
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
  *
  */
-public class CrossDefense extends CommandGroup {
+public class CrossDefense(){
     
-    public  CrossDefense(Utilities.Defense defense, boolean forward) {
-    	
-    	//Utilities.Defense defenseEnum = Utilities.Defense.values()[defense];
-    	
-    	switch(defense){
-      
-    	case LOW_BAR:
-    	  
-    		new CrossLowBar(forward).start();
-    		
-    	break;
-    	 
-    	case MOAT:
-    		
-    		new CrossMoat(forward).start();
-    		
-    	break;
-    	
-    	case ROCK_WALL:
-    		
-    		new CrossRockWall(forward).start();
-    		
-    	break;
-    	
-    	case ROUGH_TERRAIN:
-    		
-    		new CrossRoughTerrain(forward).start();
-    		
-    	break;
-    	
-    	case RAMPARTS:
-    		
-    		new CrossRamparts(forward).start();
-    		
-    	break;
-    	
-    	default:
-    		
-    		new DoNothing().start();
-    		
-    	break;
-      }
-    }
+   public static Command chooseDefense(Utilities.Defense d){
+	   
+	   switch(defenseEnum){
+	   
+	   case 0:
+		   
+		   return new CrossLowBar();
+	   
+		   break;
+	   
+	   case 1:
+		   
+		   return new CrossMoat();
+		   
+		   break;
+		   
+	   case 2:
+		   
+		   return new CrossRockWall();
+		   
+		   break;
+		   
+	   case 3:
+		   
+		   return new CrossRoughTerrain();
+	
+		   break;
+		   
+	   case 4:
+		   
+		   return new CrossRamparts();
+		   
+		   break;
+	   }
+	   
+   }
+   
 }
