107a2997e1e527907fe1f85cb83da3693575ae0d
NoahB4536 <noahbrownlee1998@gmail.com>
2016-03-22 18:56:57 -0500

    fixed issues with not being limited (switched order of throttle limits)

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/subsystems/ScissorLift.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/subsystems/ScissorLift.java
@@ -43,11 +43,11 @@ public class ScissorLift extends Subsystem {
     }
 	
 	public void safeDrive(double throttle) {
-		double tempVar = throttle;
 		throttle = Utilities.limit(throttle, -1.0, Constants.HOLD_SCISSOR_SPEED);
+		double tempVar = throttle; //setting the old throttle for the next loop through
 		throttle = Utilities.accelLimit(throttle, oldThrottle, Constants.ACCEL_LIMIT_DRIVE);
 		driveLift(throttle);
-		oldThrottle = tempVar;
+		oldThrottle = tempVar; //setting the old throttle for the next loop through
 	}
 	
 	/**
