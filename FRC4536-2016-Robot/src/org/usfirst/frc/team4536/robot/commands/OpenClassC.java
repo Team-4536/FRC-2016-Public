36dceb9b00d43b876b6eb46a51ae79235bb6f96a
audreylemeur <alemeur001@stpaul.k12.mn.us>
2016-04-02 12:24:56 -0500

    New class c auto!!!

---
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/OpenClassC.java
@@ -0,0 +1,63 @@
+package org.usfirst.frc.team4536.robot.commands;
+
+import org.usfirst.frc.team4536.robot.Constants;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class OpenClassC extends CommandGroup {
+	
+    public  OpenClassC(double distance, double velocity, double accel, double extratime, double gyro, boolean orientation, double defensesOver) {
+        
+    	addParallel(new ReleaseIntake());
+    	
+    	DriveTrapezoidProfile crossDefense;
+    	TeleopTurn turnOne;
+    	DriveTrapezoidProfile moveOver;
+    	TeleopTurn turnTwo;
+    	
+    	if (orientation){
+    		
+    		if (defensesOver > 0){
+        		turnOne = new TeleopTurn(90);
+        	}else{
+        		turnOne = new TeleopTurn(270);
+        	}
+    		
+        	crossDefense = new DriveTrapezoidProfile(distance, velocity, accel);
+        	double maxTime = crossDefense.getNeededTime() + extratime;
+    		addSequential(crossDefense, maxTime);
+    		
+    		addSequential(turnOne);
+    		
+    		moveOver = new DriveTrapezoidProfile(Constants.DISTANCE_BETWEEN_DEFENSES*defensesOver, Constants.SPEED_LIMIT, Constants.ACCEL_LIMIT_DRIVE);
+    		addSequential(moveOver);
+    		
+    		turnTwo = new TeleopTurn(180);
+    		addSequential(turnTwo);
+    		
+    	}else{
+    		
+    		if (defensesOver > 0){
+        		turnOne = new TeleopTurn(-90);
+        	}else{
+        		turnOne = new TeleopTurn(-270);
+        	}
+    		
+        	crossDefense = new DriveTrapezoidProfile(-distance, velocity, accel);
+        	double maxTime = crossDefense.getNeededTime() + extratime;
+    		addSequential(crossDefense, maxTime);
+    		
+    		addSequential(turnOne);
+    		
+    		moveOver = new DriveTrapezoidProfile(-(Constants.DISTANCE_BETWEEN_DEFENSES*defensesOver), Constants.SPEED_LIMIT, Constants.ACCEL_LIMIT_DRIVE);
+    		addSequential(moveOver);
+    		
+    		turnTwo = new TeleopTurn(0);
+    		addSequential(turnTwo);
+    	}
+    	
+    }
+}
