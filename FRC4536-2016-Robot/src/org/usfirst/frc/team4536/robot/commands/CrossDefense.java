77b99c2a96a18906aadb294c7d5d3ab09cfc1699
NoahB4536 <noahbrownlee1998@gmail.com>
2016-04-02 11:37:53 -0500

    added/commented out println statements

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/CrossDefense.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/CrossDefense.java
@@ -15,26 +15,32 @@ public class CrossDefense {
 	   
 	   case LOW_BAR:
 		   
+		   //System.out.println("CrossDefense: Low Bar");
 		   return new CrossLowBar(orientation);
 	   
 	   case MOAT:
-		   
+
+		   //System.out.println("CrossDefense: Moat");
 		   return new CrossMoat(orientation);
 		   
 	   case ROCK_WALL:
-		   
+
+		   //System.out.println("CrossDefense: Rock Wall");
 		   return new CrossRockWall(orientation);
 
 	   case ROUGH_TERRAIN:
-		   
+
+		   //System.out.println("CrossDefense: Rough Terrain");
 		   return new CrossRoughTerrain(orientation);
 	
 	   case RAMPARTS:
-		   
+
+		   //System.out.println("CrossDefense: Ramparts");
 		   return new CrossRamparts(orientation);
 		   
 	   default:
-		   
+
+		   //System.out.println("CrossDefense: Release Intake");
 		   return new ReleaseIntake();
 	   }
 	   
