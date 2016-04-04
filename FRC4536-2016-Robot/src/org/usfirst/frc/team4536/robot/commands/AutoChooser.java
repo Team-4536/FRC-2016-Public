b3de4862f4e26e56b86769dac86dee9c5f6ec7e9
Liam <liam.tomsonmoylan@gmail.com>
2016-04-04 18:03:39 -0500

    Simplified AutoChooser to work with command group efficiently

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/AutoChooser.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/AutoChooser.java
@@ -31,7 +31,6 @@ public class AutoChooser extends CommandGroup {
 	
 	boolean forward = true;
 	
-
     public AutoChooser() {
     	
     	/*-----Constructors-----*/
@@ -68,11 +67,11 @@ public class AutoChooser extends CommandGroup {
     	defensePositionChooser.addObject(" Position 5", 5);
     	SmartDashboard.putData(" Defense Position Chooser", defensePositionChooser);
     	
-    	classCChooser.addObject(" Move over -2", -2);
-    	classCChooser.addObject(" Move over -1", -1);
+    	classCChooser.addObject(" Move over left 2 (driver perspective)", -2);
+    	classCChooser.addObject(" Move over left 1 (driver perspective)", -1);
     	classCChooser.addDefault(" None", 0);
-    	classCChooser.addObject(" Move over 1", 1);
-    	classCChooser.addObject(" Move over 2", 2);
+    	classCChooser.addObject(" Move over right 1 (driver perspective)", 1);
+    	classCChooser.addObject(" Move over right 2 (driver perspective)", 2);
     	SmartDashboard.putData(" Class C Chooser", classCChooser);
     	
     	int defense = (int) autoChooser.getSelected().hashCode();
@@ -81,157 +80,121 @@ public class AutoChooser extends CommandGroup {
     	
     	int classC = (int) classCChooser.getSelected().hashCode();
     	
-    	if (pos == 1 || pos == 0) {
-        	
-    		//TODO Add orientation to new autoChooser
-	    	switch ((int) orientationChooser.getSelected().hashCode()) {
+    	switch ((int) orientationChooser.getSelected().hashCode()) {
+    	
+	    	case 0:
+	
+	    		CommandBase.driveTrain.resetNavX(0.0);
+	    		orientation = true;
+	    	break;
 	    	
-		    	case 0:
-
-		    		CommandBase.driveTrain.resetNavX(0.0);
-		    		orientation = true;
-		    	break;
-		    	
-		    	case 1:
-
-		    		CommandBase.driveTrain.resetNavX(180.0);
-		    		orientation = false;
-		    	break;
-		    	
-		    	default:
-		    		
-		    		orientation = true;
-		    	break;
-	    	}	
-	    	if (classC == 0){
+	    	case 1:
+	
+	    		CommandBase.driveTrain.resetNavX(180.0);
+	    		orientation = false;
+	    	break;
+	    	
+	    	default:
 	    		
-	    		switch (defense) {
+	    		orientation = true;
+	    	break;
+		}
+	
+		switch (defense) {
+		
+			case 0:
 			
-					case 0:
-					
-					addSequential(new ReleaseIntake());
-					
-					break;
-
-				case 1:
-
-					addSequential(new DoNothing());
-
-				break;
-
-				case 2:
-
-					addSequential(new ReachOuterWorks(orientation));
-
-				break;
+				addSequential(new ReleaseIntake());
 			
-				case 3:
-
-					addSequential(new PickUpBoulder());
+			break;
+	
+			case 1:
+	
+				addSequential(new DoNothing());
+	
+			break;
+	
+			case 2:
+	
+				addSequential(new ReachOuterWorks(orientation));
+	
+			break;
+		
+			case 3:
+	
+				addSequential(new PickUpBoulder());
+			
+			break;
+	
+			case 4:
+	
+				addSequential(new CrossLowBar(orientation));
+			
+			break;
+	
+			case 5:
+	
+				addSequential(new CrossRoughTerrain(orientation));
+			
+			break;
+	
+			case 6:
+	
+				addSequential(new CrossRockWall(orientation));
+	
+			break;
+	
+			case 7:
+	
+				addSequential(new CrossMoat(orientation));
 				
-				break;
-
-				case 4:
-
-					addSequential(CrossDefense.chooseDefense(Utilities.Defense.LOW_BAR, orientation));
+			break;
+	
+			case 8:
+	
+				addSequential(new CrossRamparts(orientation));
 				
-				break;
-
-				case 5:
-
-					addSequential(CrossDefense.chooseDefense(Utilities.Defense.ROUGH_TERRAIN, orientation));
+			break;
+	
+			case 9:
 				
-				break;
-
-				case 6:
-
-					addSequential(CrossDefense.chooseDefense(Utilities.Defense.ROCK_WALL, orientation));
-
-				break;
-
-				case 7:
-
-					addSequential(CrossDefense.chooseDefense(Utilities.Defense.MOAT, orientation));
-					
-				break;
-
-				case 8:
-
-					addSequential(CrossDefense.chooseDefense(Utilities.Defense.RAMPARTS, orientation));
-					
-				break;
-
-				case 9:
-					
-					CommandBase.driveTrain.resetNavX(135.0);
-					addSequential(new SpyBotLowGoal());
-					
-				break;
-
-				case 10:
-
-					addSequential(new LowBarLowGoal());
-					
-				break;
-
-				default: 
-
-					addSequential(new ReleaseIntake());
+				CommandBase.driveTrain.resetNavX(135.0);
+				addSequential(new SpyBotLowGoal());
 				
-				break;
-	    		}
-			}else{
-
-        		switch (defense){
-        		
-        		case 4:
-        			
-    				addSequential(new OpenClassC(Constants.CROSS_LOWBAR_DISTANCE, Constants.CROSS_LOWBAR_VELOCITY, 
-    						Constants.CROSS_LOWBAR_ACCEL_LIMIT, Constants.CROSS_LOWBAR_EXTRA_TIME, 
-    						Constants.TRAPEZOID_FORWARD_GYRO_PROPORTIONALITY, orientation, classC));
-        		break;
-        		
-    			case 5:
-    				
-    				addSequential(new OpenClassC(Constants.CROSS_MOAT_DISTANCE, Constants.CROSS_MOAT_VELOCITY,
-    						Constants.CROSS_MOAT_ACCEL_LIMIT, Constants.CROSS_MOAT_EXTRA_TIME,
-    						Constants.CROSS_MOAT_GYRO_PROPORTIONALITY, orientation, classC));
-    			break;
-    			
-    			case 6:
-    			
-    				addSequential(new OpenClassC(Constants.CROSS_ROCKWALL_DISTANCE, Constants.CROSS_RAMPARTS_VELOCITY,
-    						Constants.CROSS_ROCKWALL_ACCEL_LIMIT, Constants.CROSS_ROCKWALL_EXTRA_TIME,
-    						Constants.TRAPEZOID_FORWARD_GYRO_PROPORTIONALITY, orientation, classC));
-    			break;
-    			
-    			case 7:
-    				
-    				addSequential(new OpenClassC(Constants.CROSS_ROUGHTERRAIN_DISTANCE, Constants.CROSS_ROUGHTERRAIN_VELOCITY,
-    						Constants.CROSS_ROCKWALL_ACCEL_LIMIT, Constants.CROSS_ROUGHTERRAIN_EXTRA_TIME,
-    						Constants.TRAPEZOID_FORWARD_GYRO_PROPORTIONALITY, orientation, classC));
-    			break;
-    			
-    			case 8:
-    				
-    				addSequential(new OpenClassC(Constants.CROSS_RAMPARTS_DISTANCE, Constants.CROSS_RAMPARTS_VELOCITY,
-    						Constants.CROSS_RAMPARTS_ACCEL_LIMIT, Constants.CROSS_RAMPARTS_EXTRA_TIME,
-    						Constants.CROSS_RAMPARTS_GYRO_PROPORTIONALITY, orientation, classC));
-    			break;
-    			
-    			default:
-    				
-    				addSequential(new OpenClassC(Constants.CROSS_LOWBAR_DISTANCE,Constants.CROSS_LOWBAR_VELOCITY, 
-    						Constants.CROSS_LOWBAR_ACCEL_LIMIT, Constants.CROSS_LOWBAR_EXTRA_TIME, 
-    						Constants.TRAPEZOID_FORWARD_GYRO_PROPORTIONALITY, orientation, classC));
-        		break;
+			break;
+	
+			case 10:
+	
+				addSequential(new LowBarLowGoal());
+				
+			break;
+	
+			default: 
+	
+				addSequential(new ReleaseIntake());
+			
+			break;
+		}
+    	
+    	if (pos == 1 || pos == 0) {
+	    	
+	    	//initial mode selection
+	    	//ClassC if crossing
+	    	if (defense >= 4 && defense <= 8) { // crossing defense
+	    		
+	    		if (classC != 0) {
+	    			
+	    			addSequential(new OpenClassC(orientation, classC));
         		}	
-			}
-	    }else{
+	    	}
+	    }
+    	else{
 
 			//System.out.println("Auto Cross And Score");
-    		addSequential(new CrossNScore(defense, pos));
-
+    		if (defense >=5 && defense <= 8) {
+    			
+    			addSequential(new CrossNScore(pos));
+    		}
 	    }
     }  	
 }	
