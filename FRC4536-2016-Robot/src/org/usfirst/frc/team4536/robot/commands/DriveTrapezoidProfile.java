a507cfba6d282edf81bbeefead558c5f226dc4b3
Liam <liam.tomsonmoylan@gmail.com>
2016-04-06 17:49:14 -0500

    Uses getEncoder which gets the right encoder. Check which encoder works

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/DriveTrapezoidProfile.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/DriveTrapezoidProfile.java
@@ -79,7 +79,7 @@ public class DriveTrapezoidProfile extends CommandBase {
     
     protected void execute() {
     	
-    	driveTrain.arcadeDrive(trapezoid.throttle(timer.get()) + (Constants.TRAPEZOID_FORWARD_PROPORTIONALITY * (trapezoid.idealDistance(timer.get())*12 - driveTrain.getLeftEncoder())),
+    	driveTrain.arcadeDrive(trapezoid.throttle(timer.get()) + (Constants.TRAPEZOID_FORWARD_PROPORTIONALITY * (trapezoid.idealDistance(timer.get())*12 - driveTrain.getEncoder())),
     							(proportionalityConstant * Utilities.angleDifference(startingAngle,driveTrain.getAngle())));
     	//Ask Caleb or Mairead on the implementation of feedforward+feedback
     	
