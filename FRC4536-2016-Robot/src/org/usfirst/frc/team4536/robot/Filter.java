aa2a81de009d9062f4f04a89fdaa2dc4242dccfc
Liam <liam.tomsonmoylan@gmail.com>
2016-06-17 00:50:15 -0500

    Sample Filter base on ArrayList. Basic Mean, Median, and Mode operations

---
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/Filter.java
@@ -0,0 +1,146 @@
+package org.usfirst.frc.team4536.robot;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Filter {
+	
+	ArrayList <Double> filter;
+	double startTime = 0.0;
+	double duration = 0.0;
+	
+	/**
+	 * @author Liam
+	 * Default Constructor with a sample size of 11 data points
+	 */
+	public Filter() {
+		
+		filter = new ArrayList<Double>(Constants.DEFAULT_SAMPLE_SIZE);
+	}
+	
+	/**
+	 * @author Liam
+	 * @param sampleSize the number of data points to be sampled
+	 */
+	public Filter(int sampleSize) {
+		
+		filter = new ArrayList<Double>(sampleSize);
+	}
+	
+	/**
+	 * @author Liam
+	 * @param startTime the clock time
+	 * @param duration the duration over which data is sampled
+	 */
+	public Filter(double startTime, double duration) {
+		
+		this();
+		this.startTime = startTime;
+		this.duration = duration;
+	}
+	
+	/**
+	 * @author Liam
+	 * @param duration the duration over which data is sampled
+	 */
+	public Filter(double duration) {
+		
+		this();
+		this.duration = duration;
+	}
+	
+	/**
+	 * @author Liam
+	 * @param value the value added to the sample
+	 */
+	public void update(double value) {
+		
+		filter.add(value);
+		Collections.sort(filter);
+	}
+	
+	/**
+	 * @author Liam
+	 * @return the size of the sample
+	 */
+	public int size() {
+		
+		return filter.size();
+	}
+	
+	/**
+	 * @author Liam
+	 * @param index the index in the sample
+	 * @return the value at the index in the sample
+	 */
+	public double get(int index) {
+		
+		return filter.get(index);
+	}
+	
+	/**
+	 * @author Liam
+	 * @return the first most frequently occurring value in the sample
+	 */
+	public double getMode() {
+		
+		short modeIndex = 0;
+		short freq = 1;
+		short valFreq = 0;
+		for (int i = 1; i < filter.size(); i++) {
+			
+			if (filter.get(i).equals(filter.get(i-1))) {
+				
+				freq++;
+				
+				if (freq > valFreq) {
+					
+					valFreq = freq;
+					modeIndex = (short) (i-1);
+				}
+			}
+			else {
+				
+				if (freq > valFreq) {
+					
+					valFreq = freq;
+					modeIndex = (short) (i-1);
+				}
+				
+				freq = 1;
+			}
+		}
+		
+		return filter.get(modeIndex);
+	}
+	
+	/**
+	 * @author Liam
+	 * @return the median value of the sample
+	 */
+	public double getMedian() {
+		
+		if ((filter.size() % 2) == 1) {
+			
+			return filter.get((int)(filter.size()/2));
+		}
+		
+		return (filter.get((int) (filter.size()/2)) + filter.get((int) (filter.size()/2)-1))/2;
+	}
+	
+	/**
+	 * @author Liam
+	 * @return the average value of the sample
+	 */
+	public double getMean() {
+		
+		double sum = 0.0;
+		
+		for (int i = 0; i < filter.size(); i ++) {
+			
+			sum += filter.get(i);
+		}
+		
+		return sum / filter.size();
+	}
+}
