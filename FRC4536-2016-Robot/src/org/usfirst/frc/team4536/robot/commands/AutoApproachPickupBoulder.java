738bb259533b300c199fa4ca63fa8a4d24044f92
Liam <liam.tomsonmoylan@gmail.com>
2016-06-30 20:27:38 -0500

    Replaced values with constants. Added constructor for custom distance.

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/AutoApproachPickupBoulder.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/commands/AutoApproachPickupBoulder.java
@@ -5,15 +5,33 @@ import org.usfirst.frc.team4536.robot.Constants;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
- *
+ *@author Liam
+ *Approaches and stops when colliding with even a ball, then tries to pick it up automatically.
  */
 public class AutoApproachPickupBoulder extends CommandGroup {
     
+	/**
+	 * @author Liam
+	 */
     public  AutoApproachPickupBoulder() {
 
-    	addSequential(new BraceAgainstWall(4, 1, 1,
+    	addSequential(new BraceAgainstWall(Constants.AUTO_APPROACH_DEFAULT_DISTANCE,
+    										Constants.AUTO_APPROACH_SPEED, Constants.AUTO_APPROACH_ACCELERATION,
 											Constants.DEFAULT_CROSSING_GYRO_PROPORTIONALITY, CommandBase.driveTrain.getAngle(),
-											0.15));
+											Constants.AUTO_APPROACH_JERK_COLLISION_THRESHOLD));
+    	addSequential(new IntakeCurrent());
+    }
+    
+    /**
+     * @author Liam
+     * @param distance the distance the robot should roam for to collide with boulders and try to pick them up.
+     */
+    public AutoApproachPickupBoulder(double distance) {
+    	
+    	addSequential(new BraceAgainstWall(distance,
+				Constants.AUTO_APPROACH_SPEED, Constants.AUTO_APPROACH_ACCELERATION,
+				Constants.DEFAULT_CROSSING_GYRO_PROPORTIONALITY, CommandBase.driveTrain.getAngle(),
+				Constants.AUTO_APPROACH_JERK_COLLISION_THRESHOLD));
     	addSequential(new IntakeCurrent());
     }
 }
