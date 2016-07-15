5a10ae053e83aaf05e310155bd60c14fcff8e7a3
Liam <liam.tomsonmoylan@gmail.com>
2016-07-15 14:12:58 -0500

    added many methods and protected members so profiles possess data

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/Profile.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/Profile.java
@@ -1,7 +1,32 @@
 package org.usfirst.frc.team4536.robot;
 
+//TODO add protected members for proportionality and integral constants
 public abstract class Profile {
-
+	
+	protected double turnProportionality;
+	protected double turnIntegral;
+	protected double timeoutOffset;
+	protected double angleThreshold;
+	protected double angularVelocityThreshold;
+	
+	/**
+	 * @author Liam
+	 * @return the proportionality constant to convert from error (degrees) to throttle
+	 */
+	public abstract double getTurnProportionality();
+	
+	/**
+	 * @author Liam
+	 * @return the proportionality constant for the integral term to convert from accumulated error (degrees * time) to throttle
+	 */
+	public abstract double getTurnIntegral();
+	
+	/**
+	 * @author Liam
+	 * @return the timeout offset of the profile
+	 */
+	public abstract double getTimeoutOffset();
+	
 	/**
 	 * @author Mairead and Liam
 	 * @param time The amount of time since the profile has started
@@ -28,4 +53,16 @@ public abstract class Profile {
 	 * @return timeNeeded the theoretical time needed for the profile to execute without correction
 	 */
 	public abstract double getTimeNeeded();
+	
+	/**
+	 * @author Liam
+	 * @return the angle threshold of the profile for command termination
+	 */
+	public abstract double getAngleThreshold();
+	
+	/**
+	 * @author Liam
+	 * @return the angular velocity threshold of the profile for command termination
+	 */
+	public abstract double getAngularVelocityThreshold();
 }
