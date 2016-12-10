e4c6eeb24645e3dfd9c8dcc6e1ff7e564f44afde
audreylemeur <alemeur001@stpaul.k12.mn.us>
2016-12-10 11:44:19 -0600

    Just made this look pretty

--- FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/RobotMap.java
+++ FRC4536-2016-Robot/src/org/usfirst/frc/team4536/robot/RobotMap.java
@@ -31,12 +31,12 @@ public final class RobotMap {
     public static final int PDP_LEFT_MOTOR_FRONT = 14;
     public static final int PDP_LEFT_MOTOR_BACK = 15;
     
-    /*-------------------------------------Joystick Ports---------------------------------*/
+    /*----------------------------------------Joystick Ports---------------------------------*/
     public static final int MAIN_STICK = 0; // (Also the left stick for tank drive)
     public static final int SECONDARY_STICK = 1; // (Also the right stick for tank drive)
     public static final int TERTIARY_STICK = 2; //Scissor Lift
     
-    /*-------------------------------------Joystick Buttons---------------------------------*/
+    /*-------------------------------------Joystick Buttons----------------------------------*/
     public static final int INTAKE_BUTTON_NUMBER = 5;
     public static final int EJECT_BUTTON_NUMBER = 6;
     public static final int HOLD_BUTTON_NUMBER = 7;
@@ -52,19 +52,19 @@ public final class RobotMap {
     public static final int HOLD_AT_ANGLE_FOR_RIGHT_GOAL = 4;
     
     
-	/*-------------------------------------Relay Solenoid-----------------*/
+	/*-------------------------------------Relay Solenoid-----------------------------------*/
 	
 	public static final int SCISSOR_RELAY = 0; //these go in the relay port, 
 	public static final int INTAKE_RELAY = 1; //electricians; not PWM or DIM 
 	
-    /*-------------------------------------Digital Sensor Channels---------------------------------*/	
+    /*--------------------------------Digital Sensor Channels-------------------------------*/	
 	
 	public static final int LEFT_ENCODER_CHANNEL_A = 8; //1 yellow wire in signal for left encoder
 	public static final int LEFT_ENCODER_CHANNEL_B = 9; //Set of 3 wires blue in signal, then orange and brown for left encoder
 	public static final int RIGHT_ENCODER_CHANNEL_A = 6; //1 yellow wire in signal for right encoder
 	public static final int RIGHT_ENCODER_CHANNEL_B = 7; //Set of 3 wires blue in signal, then orange and brown for right encoder
 
-	 /*-------------------------------------Analog Sensor Channels---------------------------------*/
+	 /*--------------------------------Analog Sensor Channels-------------------------------*/
 	public static final int FRONT_ULTRA_CHANNEL = 0;
 	public static final int BACK_ULTRA_CHANNEL = 1;
 	public static final int LEFT_IR_CHANNEL = 2;
